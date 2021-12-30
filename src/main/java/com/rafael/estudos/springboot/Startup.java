ad33a32c819a5f0c784e97b42a166130bf5dd030
rafalanutti.dsn.imb <rafalanutti.dsn.imb@alterdata.com>
2021-12-30 12:55:22 -0300

    Associação ManyToMany produto x categoria

--- src/main/java/com/rafael/estudos/springboot/Startup.java
+++ src/main/java/com/rafael/estudos/springboot/Startup.java
@@ -1,13 +1,48 @@
 package com.rafael.estudos.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafael.estudos.springboot.domain.Categoria;
+import com.rafael.estudos.springboot.domain.Produto;
+import com.rafael.estudos.springboot.repository.CategoriaRepository;
+import com.rafael.estudos.springboot.repository.ProdutoRepository;
+
 @SpringBootApplication
-public class Startup {
+public class Startup implements CommandLineRunner {
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria c1 = new Categoria(null, "INFORMATICA");
+		Categoria c2 = new Categoria(null, "ESCRITÓRIO");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- src/main/java/com/rafael/estudos/springboot/domain/Categoria.java
+++ src/main/java/com/rafael/estudos/springboot/domain/Categoria.java
@@ -1,12 +1,16 @@
 package com.rafael.estudos.springboot.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -16,6 +20,17 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	public Categoria(Integer id, String nome) {
 		super();
