feb63ae0e35d0a64196754688210d855339ce845
rafalanutti.dsn.imb <rafalanutti.dsn.imb@alterdata.com>
2022-01-02 11:37:34 -0300

    Validação de email unico no cliente

--- src/main/java/com/rafael/estudos/springboot/repository/ClienteRepository.java
+++ src/main/java/com/rafael/estudos/springboot/repository/ClienteRepository.java
@@ -2,9 +2,16 @@ package com.rafael.estudos.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rafael.estudos.springboot.domain.Cliente;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	/**
+	 * Spring Data Padrão de nomes, spring detecta que quer uma busca pelo email
+	 */
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/rafael/estudos/springboot/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/rafael/estudos/springboot/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.rafael.estudos.springboot.domain.enums.TipoCliente;
 import com.rafael.estudos.springboot.dto.ClienteNewDTO;
 import com.rafael.estudos.springboot.exception.FieldMessage;
+import com.rafael.estudos.springboot.repository.ClienteRepository;
 import com.rafael.estudos.springboot.utils.ValidatorCpfCnpj;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repository;
 
 	@Override
 	public void initialize(ClienteInsert constraintAnnotation) {
@@ -31,6 +37,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				&& !ValidatorCpfCnpj.isCNPJ(obj.getCpfOuCnpj())) {
 			messages.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 
+		}
+
+		var clienteExiste = repository.findByEmail(obj.getEmail());
+
+		if (clienteExiste != null) {
+			messages.add(new FieldMessage("email", "Email já cadastrado"));
 		}
 		for (FieldMessage f : messages) {
 			context.disableDefaultConstraintViolation();
