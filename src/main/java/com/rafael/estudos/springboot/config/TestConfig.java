b1e320da69a187af24b29440db5761232802ebfb
rafalanutti.dsn.imb <rafalanutti.dsn.imb@alterdata.com>
2022-01-06 20:08:45 -0300

    Criando profile de teste

---
+++ src/main/java/com/rafael/estudos/springboot/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.rafael.estudos.springboot.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.rafael.estudos.springboot.service.DbService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DbService service;
+
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+
+		service.instantiateDataBase();
+		return true;
+	}
+
+}
