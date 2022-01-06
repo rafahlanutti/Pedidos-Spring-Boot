cdf9e4fb6370e0b9ec375f105b08198e1b082660
rafalanutti.dsn.imb <rafalanutti.dsn.imb@alterdata.com>
2022-01-06 20:29:34 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/rafael/estudos/springboot/config/DevConfig.java
+++ src/main/java/com/rafael/estudos/springboot/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.rafael.estudos.springboot.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,17 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.rafael.estudos.springboot.service.DbService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DbService service;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
 
-		service.instantiateDataBase();
+		if (strategy.equals("create")) {
+			service.instantiateDataBase();
+
+		}
 		return true;
 	}
 
