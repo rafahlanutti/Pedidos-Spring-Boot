b41dbb7984cc47a38ef0b1195be110ff3f5be7c8
rafalanutti.dsn.imb <rafalanutti.dsn.imb@alterdata.com>
2022-01-18 20:54:50 -0300

    Implementando autenticação

--- src/main/java/com/rafael/estudos/springboot/config/SecurityConfig.java
+++ src/main/java/com/rafael/estudos/springboot/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.rafael.estudos.springboot.security.JWTAuthenticationFilter;
+import com.rafael.estudos.springboot.utils.JWTUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
+
+	@Autowired
+	private UserDetailsService userDetailService;
+
+	@Autowired
+	private JWTUtils jwtUtils;
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
@@ -37,6 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeHttpRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtils));
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	@Bean
---
+++ src/main/java/com/rafael/estudos/springboot/utils/JWTUtils.java
@@ -0,0 +1,26 @@
+package com.rafael.estudos.springboot.utils;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JWTUtils {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Value("${jwt.expiration}")
+	private Long expiration;
+
+	public String generateToken(String username) {
+
+		return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secret.getBytes()).compact();
+	}
+	
+	
+}
